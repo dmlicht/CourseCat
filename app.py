7caa937b96c112052c32219cb089cebd95e5c2b3
Alyssa Frazee <acfrazee@gmail.com>
2013-07-01 17:44:02 -0400

    added form to webpage

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 #! usr/local/bin/python
 
-from flask import Flask, session, g, config, render_template
+from flask import Flask, session, g, config, render_template, request
+from flask.ext.wtf import Form, TextField, ValidationError, Required
 
 
 DEBUG = True
 app = Flask(__name__)
 
 app.config.from_object(__name__)
+app.config['SECRET_KEY'] = 'devkey'
 
-@app.route('/')
+
+class SubmitForm(Form):
+    name = TextField('Name', description='Short title of course/tutorial')
+    url = TextField('URL', description='Where can the course/tutorial be found on the web?')
+
+@app.route('/', methods = ('GET', 'POST',))
 def home():
-    return render_template('home.html')
+	form = SubmitForm()
+	return render_template('home.html', form=form)
 
 if __name__ == '__main__':
     app.run(port=5001)
