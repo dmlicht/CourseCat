fe447b827805562d3b7cb432a589ff6b85ef21cb
Alyssa Frazee <acfrazee@gmail.com>
2013-07-01 18:18:11 -0400

    debugged database integration

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
 #! usr/local/bin/python
 from flask.ext.wtf import Form, TextField, ValidationError, Required
 import sqlite3
+from pprint import pprint
 from flask import Flask, session, g, config, render_template, request, redirect, url_for
 
-
-DEBUG = True
-DATABASE = 'coursecat.db'
 app = Flask(__name__)
+DEBUG = True
+app.config['DATABASE'] = 'coursecat.db'
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = 'devkey'
+
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
 def init_db():
-    with open(connect_db()) as db:
+    with connect_db() as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
 if DEBUG:
     init_db()
-
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = 'devkey'
-
 
 class SubmitForm(Form):
     name = TextField('Name', description='Short title of course/tutorial')
@@ -33,24 +32,35 @@ def before_request():
     g.db = connect_db()
 
 @app.teardown_request
-def teardown_request():
+def teardown_request(Exception):
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
 
 @app.route('/')
 def home():
-	form = SubmitForm()
+    form = SubmitForm()
     cursor = g.db.execute('select name, url, description from courses order by id desc')
     courses = [dict(name=row[0], url=row[1], description=row[2]) for row in cursor.fetchall()]
-    return render_template('home.html', courses=courses, form=form)
+    pprint(courses)
+    return render_template('courselist.html', courses=courses, form=form)
 
-@app.route('/courses/add', method="POST")
+@app.route('/courses/add', methods=["GET","POST"])
 def post_course():
+    print 'getitng a request'
+    pprint(request.form)
     g.db.execute('insert into courses (name, url, description) values (?, ?, ?)', 
-            request.form['name'], request.form['url'], request.form['description'])
+             (request.form['name'], request.form['url'], "description"))
     g.db.commit()
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(port=5001)
+
+
+
+
+
+
+
+
