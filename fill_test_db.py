cce37cbf0dcc72a0a5cd26c06ff4c31e90f1a52c
Alyssa Frazee <acfrazee@gmail.com>
2013-07-11 00:18:05 -0400

    added function in test database file to create topics that have courses attached to them

--- fill_test_db.py
+++ fill_test_db.py
@@ -1,5 +1,6 @@
 from coursecat import db
 from coursecat.models import *
+db.create_all()
 import random
 
 TEST_TOPIC_NAMES = [
@@ -22,9 +23,9 @@ def create_and_add_course(num=-1):
 	if num == -1:
 		num = random.randint(0,1000)
 	new_course = Course(
-			name="course_%d" % num, 
-			url="course_%d_url" %num, 
-			description="course_%d_description" % num
+			name = "course_%d" % num, 
+			url = "course_%d_url" %num, 
+			description = "course_%d_description" % num
 	)
 	db.session.add(new_course)
 
@@ -36,7 +37,29 @@ def create_and_add_topic(name):
 	)
 	db.session.add(new_topic)
 
+def create_and_add_topic_with_courses(name):
+	import random
+	num = random.randint(0,1000)
+	new_course = Course(
+		name = 'course_%d' % num,
+		url = 'www.awesome.com',
+		description = 'description here'
+	)
+	new_topic = Topic(name=name)
+	db.session.add(new_topic)
+	db.session.add(new_course)
+	new_topic.courses.append(new_course)
+
+	second_course = Course(
+		name = 'courseII_%d' % num,
+		url = 'www.awesomesquared.com',
+		description = 'description here AGAIN!'
+	)
+
+	new_topic.courses.append(second_course)
+
 map(create_and_add_course, xrange(10))
-map(create_and_add_topic, TEST_TOPIC_NAMES)
+map(create_and_add_topic, TEST_TOPIC_NAMES[:4])
+map(create_and_add_topic_with_courses, TEST_TOPIC_NAMES[4:])
 
 db.session.commit()
