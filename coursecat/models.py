89d06c770c6b8b23e0fce22c4d2f8826f02fca8a
Alyssa Frazee <acfrazee@gmail.com>
2013-07-11 00:16:35 -0400

    fixed association table so that courses and topics are connected

--- coursecat/models.py
+++ coursecat/models.py
@@ -1,33 +1,51 @@
 from coursecat import db
-from sqlalchemy.ext.associationproxy import association_proxy
+#from sqlalchemy.ext.associationproxy import association_proxy
+
+
+topics = db.Table('topics',
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id')),
+    db.Column('topic_id', db.Integer, db.ForeignKey('topic.id'))
+)
+
 
 class Course(db.Model):
-    """gives your object special properties, says Nina"""
+    """create new course"""
+    __tablename__ = 'course'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(140))
     url = db.Column(db.String(1000))
     description = db.Column(db.String(5000))
-    topics = association_proxy("assoc","topic")
+    #topics = db.relationship("Assoc", backref="course")
+    topics = db.relationship('Topic', secondary=topics, backref=db.backref('courses'))
 
     def __repr__(self):
         return self.name
+
+    def __init__(self, name, url, description):
+        self.name = name
+        self.url = url
+        self.description = description
+
+
 
 class Topic(db.Model):
     """categories of courses"""
+    __tablename__ = 'topic'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(140))
     description = db.Column(db.String(5000))
-    courses = association_proxy("assoc", "course")
 
     def __repr__(self):
         return self.name
 
+    def __init__(self, name):
+        self.name = name
+
 
 # association table:
-class Assoc(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
-    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
-    course = db.relation(Course, backref = "assoc")
-    topic = db.relation(Topic, backref = "assoc")
-    score = db.Column(db.Integer)
+#class Assoc(db.Model):
+#    __tablename__ = 'association'
+#    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), primary_key=True)
+#    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'), primary_key=True)
+#    topic = db.relationship("Topic", backref = "course_assocs")
+#    score = db.Column(db.Integer)
