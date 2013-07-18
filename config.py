1d06978e86f4af3a66c02164196cb4ed0c73e143
Alyssa Frazee <acfrazee@gmail.com>
2013-07-18 16:05:27 -0400

    fixed database problem with heroku

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'coursecat_working.db')
+SQLITE_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'coursecat_working.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', SQLITE_DATABASE_URI)
 SECRET_KEY = 'devkey'
--- coursecat/__init__.py
+++ coursecat/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-#import os
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -8,9 +7,3 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from coursecat import views
-
-## we had to run the following in an interpreter:
-# from coursecat import db
-# from coursecat.models import *
-# db.create_all()
-##### first time only
