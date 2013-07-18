cb714a0ea8c3bf6c820164ba21f4e05639e96b5f
Alyssa Frazee <acfrazee@gmail.com>
2013-07-18 16:07:55 -0400

    fixed problem with app call

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
 SQLITE_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'coursecat_working.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', SQLITE_DATABASE_URI)
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', SQLITE_DATABASE_URI)
 SECRET_KEY = 'devkey'
