408f84f420795044416eb4c20191e821313a4c85
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-08-01 11:06:59 -0400

    changed host to zeros, added debug statement to config.py file

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 _basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'coursecat_working.db')
 SECRET_KEY = 'devkey'
+DEBUG = True
